6c64552fa0ab0e7b053fd45c393318c235e978bc
toannguyen2603 <huutoan2603@gmail.com>
2021-11-03 11:19:58 +0700

    Test structure login form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import LoginForm from "./components/authentication/Login/LoginForm";
+import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <LoginForm/>
+    </BrowserRouter>
   );
 }
 
